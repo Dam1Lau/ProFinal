12f66db1f2082ab5abfe4d7f20ba68e20ca5e4b2
Laura <Laura@Laura-PC>
2018-05-03 19:06:25 +0200

    Añadidas algunas clases básicas. Falta utilidades ficheros, Utilidades teclado.

---
+++ Programacion Proyecto Final/src/programacion/proyecto/pkgfinal/Teclado.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion.proyecto.pkgfinal;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Laura
+ */
+public class Teclado {
+
+    public static int pedirNumero() {
+        int numero = 0;
+        boolean esNumero = false;
+        Scanner lector = new Scanner(System.in);
+        while (esNumero == false) {
+            try {
+                numero = lector.nextInt();
+                esNumero = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Dato incorecto. Introduce un número porfavor.");
+            }
+        }
+        return numero;
+    }
+
+    public static int pedirPrecio() {
+        int precio = 0;
+        boolean esCorrecta = false;
+        Scanner lector = new Scanner(System.in);
+        while (esCorrecta == false) {
+            try {
+                do {
+                    System.out.println("Introduce un precio: (mínimo 5€)");
+                    precio = lector.nextInt();
+                    esCorrecta = true;
+                } while (precio < 5);
+            } catch (InputMismatchException e) {
+                System.out.println("Dato incorecto. Introduce un número porfavor.");
+            }
+        }
+        return precio;
+    }
+
+    public static boolean pedirBoolean() {
+        boolean opcion;
+        System.out.println("1. Si  2. No");
+        int numero = 0;
+        while (numero != 1 && numero != 2) {
+            numero = Teclado.pedirNumero();
+        }
+        if (numero == 1) {
+            opcion = true;
+        } else {
+            opcion = false;
+        }
+        return opcion;
+    }
+
+
+    public static Comision crearComision() {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("========== DATOS PARA CREAR UNA NUEVA COMISIÓN ============");
+        System.out.println("Código de la comisión:");
+        String cod = lector.nextLine();
+        System.out.println("Título de la comisión:");
+        String tit = lector.nextLine();
+        System.out.println("Descripción del dibujo/producto:");
+        String des = lector.nextLine();
+        System.out.println("Precio base (minimo 5€):");
+        int pre = pedirPrecio();
+        System.out.println("¿Puede ser de contenido adulto la ilustración a encargar?");
+        boolean nsfw = pedirBoolean();
+        System.out.println("Artista que vende la comisión:");
+        String artist = lector.nextLine();
+        System.out.println("¿Ilustracion hecha por medio Digital o Tradicional? 1. Digital 2. Tradicional");
+        int opcion;
+        do {
+            opcion = Teclado.pedirNumero();
+        } while (opcion != 1 && opcion != 2);
+        Comision comi;
+        if (opcion == 1) {
+            System.out.println("¿Es una ilustracion animada? 1.Si   2.No");
+            boolean anim = pedirBoolean();
+            System.out.println("¿Se enviarán los bocetos previos para hacer correcciones o solo el producto final?  1. Con correcciones   2. Solo el producto final.");
+            boolean correc = pedirBoolean();
+            comi = new Digital(anim, correc, cod, tit, des, pre, nsfw, artist);
+        } else {
+            System.out.println("Tamaño del papel o soporte a usar: (Grande, mediano, pequeño");
+            String tamano = lector.nextLine();
+            System.out.println("Zona de envio que desea abarcar: 1. UE  2. Internacional.");
+            int zona = 0;
+            do {
+                zona = Teclado.pedirNumero();
+            } while (zona != 1 && zona != 2);
+            comi = new Tradicional(tamano, zona, cod, tit, des, pre, nsfw, artist);
+        }
+        return comi;
+    }
+}
